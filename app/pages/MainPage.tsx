0732fa0d41311482fdea78cc93ebafb486d70582
Sacha Bron <me@sachabron.ch>
2018-11-25 00:02:09 +0100

    Add mic support (to be tested)

--- app/pages/MainPage.tsx
+++ app/pages/MainPage.tsx
@@ -11,128 +11,140 @@ import { Card, Intent, NonIdealState, Spinner, Text } from '@blueprintjs/core';
 import { Result, ResultProps } from './result/Result';
 
 import { ResultNode } from '../types';
+import { Mic } from "../microphone/mic";
 
 const styles = require('./MainPage.scss');
 
 export interface MainPageProps extends RouteComponentProps<any> {
-  mic: MicState;
+	mic: MicState;
 }
 
 enum SearchState {
-  INIT,
-  SPEAKING,
-  FETCHING,
-  DONE
+	INIT,
+	SPEAKING,
+	FETCHING,
+	DONE
 }
 
 interface MainPageState {
-  searchState: SearchState;
-  results: Array<ResultNode>;
+	searchState: SearchState;
+	results: Array<ResultNode>;
 }
 
 class MainPage extends React.Component<MainPageProps, MainPageState> {
-  readonly state: MainPageState = {
-    searchState: SearchState.INIT,
-    results: []
-  };
+	microphone: Mic;
 
-  setResults(results: MainPageState['results']) {
-    this.state.results = results;
-  }
+	readonly state: MainPageState = {
+		searchState: SearchState.INIT,
+		results: []
+	};
 
-  render() {
-    return (
-      <div className={styles.container}>
-        <div className={styles.content}>
-          {this.renderContent()}
-        </div>
-      </div>
-    );
-  }
+	setResults(results: MainPageState['results']) {
+		this.state.results = results;
+	}
 
-  private renderContent = () => {
-    const context = 'Stack Overflow';
-    const understood = 'I want to...';
+	componentDidMount() {
+		let elem: HTMLElement = document.querySelector('.mic-icon') as HTMLElement;
+		this.microphone = new Mic(elem, () => {
+			this.setState({ searchState: SearchState.SPEAKING });
+		}, (res) => {
+			console.log(res);
+		});
+	}
 
-    const lorem = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.' +
-        'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s,' +
-        'when an unknown printer took a galley of type and scrambled it to make a type specimen book.' +
-        'It has survived not only five centuries, but also the leap into electronic typesetting,' +
-        'remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset' +
-        'sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker' +
-        'including versions of Lorem Ipsum.';
+	render() {
+		return (
+			<div className={styles.container}>
+				<div className={styles.content}>
+					{this.renderContent()}
+				</div>
+			</div>
+		);
+	}
 
-    const results = [
-      { source: 'Stack Overflow', content: lorem },
-      { source: 'Lol wikia', content: lorem }
-    ];
+	private renderContent = () => {
+		const context = 'Stack Overflow';
+		const understood = 'I want to...';
 
-    switch (this.state.searchState) {
-      case SearchState.INIT:
-        return (
-            <NonIdealState
-                icon="headset"
-                title="Speak to search content"
-                description="Plug a microphone and speak to begin"
-            />
-        );
-      case SearchState.SPEAKING:
-        return (
-          <>
-            <Text>Keep speaking! >:3</Text>
-            <div>La super barre de Sachdr</div>
-          </>
-        );
-      case SearchState.FETCHING:
-        return (
-            <>
-              {this.renderResultHead(context, understood)}
-              {this.renderResults(results, true)}
-            </>
-        );
-      case SearchState.DONE:
-        return (
-            <>
-              {this.renderResultHead(context, understood)}
-              {this.renderResults(results, false)}
-            </>
-        );
-    }
-  }
+		const lorem = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.' +
+				'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s,' +
+				'when an unknown printer took a galley of type and scrambled it to make a type specimen book.' +
+				'It has survived not only five centuries, but also the leap into electronic typesetting,' +
+				'remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset' +
+				'sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker' +
+				'including versions of Lorem Ipsum.';
 
-  private renderResultHead = (context: string, understood: string) => (
-      <>
-        <Text>Your query for the context {context}:</Text>
+		const results = [
+			{ source: 'Stack Overflow', content: lorem },
+			{ source: 'Lol wikia', content: lorem }
+		];
 
-        <Card className={styles.understood}>
-          <Text ellipsize={true}>{understood}</Text>
-        </Card>
-      </>
-  )
+		switch (this.state.searchState) {
+			case SearchState.INIT:
+				return (
+						<NonIdealState
+								icon="headset"
+								title="Speak to search content"
+								description="Plug a microphone and speak to begin"
+						/>
+				);
+			case SearchState.SPEAKING:
+				return (
+					<>
+						<Text>Keep speaking! >:3</Text>
+						<div>La super barre de Sachdr</div>
+					</>
+				);
+			case SearchState.FETCHING:
+				return (
+						<>
+							{this.renderResultHead(context, understood)}
+							{this.renderResults(results, true)}
+						</>
+				);
+			case SearchState.DONE:
+				return (
+						<>
+							{this.renderResultHead(context, understood)}
+							{this.renderResults(results, false)}
+						</>
+				);
+		}
+	}
 
-  private renderResults = (results: ResultProps[], fetching: boolean) => {
-    const bestGuess = '350 gold';
-    return (
-        <div className={styles.results}>
-          {fetching ? <Spinner intent={Intent.PRIMARY} size={Spinner.SIZE_STANDARD}/> : (
-            <>
-              <Text className={styles.best_guess}>Best guess: {bestGuess}</Text>
-              {results.map((el: ResultProps, index: number) => <Result key={index} {...el}/>)}
-            </>
-          )}
-        </div>
-    );
-  }
+	private renderResultHead = (context: string, understood: string) => (
+			<>
+				<Text>Your query for the context {context}:</Text>
+
+				<Card className={styles.understood}>
+					<Text ellipsize={true}>{understood}</Text>
+				</Card>
+			</>
+	)
+
+	private renderResults = (results: ResultProps[], fetching: boolean) => {
+		const bestGuess = '350 gold';
+		return (
+				<div className={styles.results}>
+					{fetching ? <Spinner intent={Intent.PRIMARY} size={Spinner.SIZE_STANDARD}/> : (
+						<>
+							<Text className={styles.best_guess}>Best guess: {bestGuess}</Text>
+							{results.map((el: ResultProps, index: number) => <Result key={index} {...el}/>)}
+						</>
+					)}
+				</div>
+		);
+	}
 }
 
 function mapStateToProps(state: IState): Partial<MainPageProps> {
-  return {
-    mic: state.mic
-  };
+	return {
+		mic: state.mic
+	};
 }
 
 function mapDispatchToProps(dispatch: Dispatch<IState>): Partial<MainPageProps> {
-  return bindActionCreators(MicActions as any, dispatch);
+	return bindActionCreators(MicActions as any, dispatch);
 }
 
 export default (connect(mapStateToProps, mapDispatchToProps)(MainPage) as any as React.StatelessComponent<MainPageProps>);
